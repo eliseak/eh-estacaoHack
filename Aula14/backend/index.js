b1f5663f2e43897d33e3e3951157757233c05790
Elise AK <eliseakemi@gmail.com>
2018-06-21 12:03:37 -0300

    Adicionar acesso aos dados enviados pelo usuario e exercicio a ser feito

--- Aula14/backend/index.js
+++ Aula14/backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express'); // verifica no node_modules?
+const bodyParser = require('body-parser'); //necessário para ler os dados que o usuário enviar - bodyparser  vem junto com express
 
 const app = express(); // Aplicação para trabalhar a resposta dos clientes
+
+app.use(bodyParser.json()); // deve ser colocado antes dos endpoints
 
 // get(endereço, função de resp(requisição, resposta))
 app.get('/', (req, res) => {
@@ -11,16 +14,31 @@ app.get('/pudim', (req, res) => {
     res.send('Sorvete é melhor');
 });
 
-app.get('/teste', (req, res) => {
-    res.send('<h1>Teste</h1>');
-});
-
 app.get('/sorvete', (req, res) => {
     res.send({
         sabor: 'baunilha',
         cobertura: 'caramelo'
     });
 });
+
+app.get('/teste', (req, res) => {
+    res.send('<h1>Teste</h1>');
+});
+
+app.post('/', (req, res) =>{
+    res.send(`Recebi um POST`);
+})
+
+app.post('/lerPOST', (req, res) =>{
+    res.send(req.body);
+})
+
+app.post('/lerPOST2', (req, res) =>{
+    if (!req.body.pudim)
+        res.send('Você não tem pudim');
+    else
+        res.send(`Seu pudim é ${req.body.pudim}`);
+})
 
 app.listen(3000); //porta que o processo vai rodar
 // Sempre no final do código
@@ -31,3 +49,4 @@ app.listen(3000); //porta que o processo vai rodar
 //node index.js
 //localhost:3000
 
+// .ejs
