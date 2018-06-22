d025a6637da046c39f45b0dda69f61528e5dcb07
Elise AK <eliseakemi@gmail.com>
2018-06-22 15:20:47 -0300

    Adicionar a solucao do exercicio 1 de Node.js

--- Aula14/exercicio/exercicio1/exercicio1.js
+++ Aula14/exercicio/exercicio1/exercicio1.js
@@ -4,5 +4,30 @@ const Excel = require("exceljs");
 const arquivo = require("./arquivo.json");
 
 let workbook = new Excel.Workbook();
-
 let sheet = workbook.addWorksheet(arquivo.titulo);
+
+sheet.columns = [
+    { header: 'Nome', key: 'nome'},
+    { header: 'Idade', key: 'idade'},
+    { header: 'Profissão', key: 'profissao'},
+    { header: 'Força', key: 'FOR'},
+    { header: 'Constituição', key: 'CON'},
+    { header: 'Poder', key: 'POD'},
+    { header: 'Destreza', key: 'DES'},
+    { header: 'Aparência', key: 'APA'},
+    { header: 'Tamanho', key: 'TAM'},
+    { header: 'Inteligência', key: 'INT'},
+    { header: 'Educação', key: 'EDU'}
+];
+
+for (let col of sheet.columns){
+    col.width = 12;
+}
+
+sheet.addRows(arquivo.personagens);
+
+workbook.xlsx.writeFile("Lista de Personagens.xlsx").then(function() {
+    
+    console.log("Arquivo salvo!");
+  });
+
